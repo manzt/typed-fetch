e3a537331a4c43349e24ab3e8166fa8a27cf7bac
Trevor Manz <trevor.j.manz@gmail.com>
2022-10-14 00:10:26 -0400

    feat: initial strictly-typed body

--- mod.ts
+++ mod.ts
@@ -2,37 +2,59 @@ interface TypedFetchOptions {
 	base?: string | URL;
 }
 
-export function createTypedFetch<Paths>(
+export function createTypedFetch<Paths extends Record<string, any>>(
 	options: TypedFetchOptions = {},
 ): TypedFetch<Paths> {
+	// @ts-expect-error
 	return (input, init) => {
 		// @ts-expect-error
-		return fetch(new URL(input, options.base), init) as any;
+		return fetch(new URL(input, options.base), init);
 	};
 }
 
-type TypedResponse<Status extends PropertyKey> =
+type TypedResponse<Status, Response> =
 	& Omit<Response, "status" | "json">
-	& { status: Status };
+	& { status: Status }
+	& (Response extends JsonContent<infer T> ? { json(): Promise<T> } : unknown);
 
-type TypedJsonResponse<Status extends PropertyKey, T> =
-	& TypedResponse<Status>
-	& { json(): Promise<T> };
-
-type TypedRequestInit<Method> = Omit<RequestInit, "method"> & {
-	method: Method;
-};
+type TypedRequestInit<Method, Operation> =
+	& Omit<RequestInit, "method" | "body">
+	& { method: Method }
+	& (Operation extends { requestBody: JsonContent<infer T> } ? { body: T }
+		: unknown);
 
 type TypedFetch<Paths> = <
 	Method extends keyof Paths[Path],
 	Path extends keyof Paths,
 >(
 	input: Path,
-	init: TypedRequestInit<Method>,
-) => Paths[Path][Method] extends { responses: infer R } ? {
-		[Status in keyof R]: R[Status] extends { content: infer C }
-			? C extends { "application/json": infer T } ? Promise<TypedJsonResponse<Status, T>>
-			: Promise<TypedResponse<Status>>
-			: never;
+	init: TypedRequestInit<
+		Method & string,
+		Paths[Path][Method]
+	>,
+) => InferResponse<Paths[Path][Method]>;
+
+type InferResponse<Operation> = Operation extends { responses: infer R } ? {
+		[Status in keyof R]: Promise<
+			TypedResponse<NarrowStatus<Status, keyof R>, R[Status]>
+		>;
 	}[keyof R]
 	: never;
+
+type NarrowStatus<Status, All> = Status extends "default"
+	? Exclude<HttpStatusCode, All>
+	: Status;
+
+// deno-fmt-ignore
+type HttpStatusCode =
+	| 100
+	| 200 | 201 | 204 | 206
+	| 301 | 302 | 303 | 304 | 307 | 308
+	| 401 | 403 | 404 | 406 | 407 | 409 | 410 | 412 | 416 | 418 | 425 | 451
+	| 500 | 501 | 502 | 503 | 504;
+
+interface JsonContent<T> {
+	content: {
+		"application/json": T;
+	};
+}
